f38ec0a92e05fc022f716a31ce840ab330bc6502
d3sm0 <simone.totaro@gmail.com>
2021-03-21 12:48:36 +0100

    [CLUSTER] same_trajectory

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import getpass
 import sys
 
 import experiment_buddy
@@ -11,19 +12,21 @@ sweep_yaml = "sweep_seeds.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""  # in host
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 
+user = getpass.getuser()
 learning_rate = 1e-3
 gamma = 0.99
 eps_clip = 0.1
-opt_epochs = 10
-horizon = 256 if DEBUG else 2048
-batch_size = 32
-eta = 0.01
+opt_epochs = 1
+horizon = int(1e4)
+batch_size = horizon
+eta = 0.
 grid_size = 8
 agent = "pg"
 save_interval = 100
 max_steps = int(5e5)
 seed = 984
 h_dim = 32
+eval_runs = 10
 # wandb_mode = "online" if DEBUG else "offline"
 
 use_cuda = False
