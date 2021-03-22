c3377e99f27155791b9b1bf7043d80ab811346c8
d3sm0 <simone.totaro@gmail.com>
2021-03-22 23:35:00 +0100

    [CLUSTER] grid

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import sys
 import experiment_buddy
 import torch
 
-RUN_SWEEP = 1
+RUN_SWEEP = 10
 REMOTE = 1
-NUM_PROCS = 20
+NUM_PROCS = 10
 
 sweep_yaml = "sweep_seeds.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""  # in host
@@ -21,9 +21,9 @@ horizon = 256 if DEBUG else 2048
 batch_size = 32
 eta = 0.1
 grid_size = 8
-agent = "ppo"
+agent = "pg"
 save_interval = 10
-max_steps = int(5e5)
+max_steps = int(300)
 seed = 984
 h_dim = 32
 # wandb_mode = "online" if DEBUG else "offline"
