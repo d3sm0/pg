43d73b9634fb504bef045b08419580130ab4418a
d3sm0 <simone.totaro@gmail.com>
2021-03-22 12:00:20 +0100

    [CLUSTER] local

--- config.py
+++ config.py
@@ -1,29 +1,33 @@
+import getpass
 import sys
 
 import experiment_buddy
 import torch
 
-RUN_SWEEP = 1
+DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 REMOTE = 1
-NUM_PROCS = 20
+RUN_SWEEP = REMOTE
+NUM_PROCS = 4 if not DEBUG else 1
+HOST = "mila" if REMOTE else ""  # in host
 
 sweep_yaml = "sweep_seeds.yaml" if RUN_SWEEP else False
-HOST = "mila" if REMOTE else ""  # in host
-DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 
+user = getpass.getuser()
 learning_rate = 1e-3
+# lr_decay = 0.99
 gamma = 0.99
 eps_clip = 0.1
 opt_epochs = 10
-horizon = 256 if DEBUG else 2048
+horizon = 200 if DEBUG else int(1e4)
 batch_size = 32
-eta = 0.01
+eta = 0.
 grid_size = 8
-agent = "pg"
-save_interval = 100
-max_steps = int(5e5)
+agent = "ppo"
+save_interval = 50
+max_steps = int(200)
 seed = 984
 h_dim = 32
+eval_runs = 10
 # wandb_mode = "online" if DEBUG else "offline"
 
 use_cuda = False
