e843c382d766686597b044118fb2017e737e7ba4
d3sm0 <simone.totaro@gmail.com>
2021-03-22 13:15:46 +0100

    [CLUSTER] linear

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import itertools
 
 import torch
 from gym_minigrid.envs import EmptyEnv
-
+from eval_policy import eval_policy
 import config
 from agent import PG, PPO
 from env_utils import MiniGridWrapper
@@ -28,22 +28,30 @@ def main():
     env.seed(config.seed)
     env = MiniGridWrapper(env)
     if config.agent == "pg":
-        agent = PG(action_space=env.action_space.n, observation_space=env.observation_space.shape[0], h_dim=config.h_dim)
+        agent = PG(action_space=env.action_space.n, observation_space=env.observation_space.shape[0],
+                   h_dim=config.h_dim)
     else:
-        agent = PPO(action_space=env.action_space.n, observation_space=env.observation_space.shape[0], h_dim=config.h_dim)
+        agent = PPO(action_space=env.action_space.n, observation_space=env.observation_space.shape[0],
+                    h_dim=config.h_dim)
 
     # writer = tb.SummaryWriter(log_dir=f"logs/{dtm}_as_ppo:{config.as_ppo}")
-    for global_step in itertools.count():
-        info = gather_trajectory(env, agent, config.horizon)
-        config.tb.add_scalar("return", info["env/returns"], global_step=global_step * config.horizon)
+    # for global_step in itertools.count():
+    global_step = 0
+    info = gather_trajectory(env, agent, config.horizon)
+    # config.tb.add_scalar("return", info["env/returns"], global_step=global_step * config.horizon)
+    for global_step in range(config.max_steps):
         losses = agent.train()
-        agent.data.clear()
+        # agent.data.clear()
         for k, v in losses.items():
-            config.tb.add_scalar(k, v, global_step=global_step * config.horizon)
+            config.tb.add_scalar(k, v, global_step=global_step)
         if global_step % config.save_interval == 0:
-            config.tb.add_object("agent", agent.get_model(), global_step=0)
-        if (global_step * config.horizon) > config.max_steps:
-            break
+            path = config.tb.add_object("agent", agent.get_model(), global_step=0)
+            # if config.user == "d3sm0":
+            eval_info = eval_policy(path, config.eval_runs)
+            for k, v in eval_info.items():
+                config.tb.add_scalar(k, v, global_step=global_step)
+    # if (global_step * config.horizon) > config.max_steps:
+    #    break
     env.close()
 
 
