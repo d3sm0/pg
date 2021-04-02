686a4b15db93e787b80f7139030a4f7a752ca0c5
d3sm0 <simone.totaro@gmail.com>
2021-04-02 03:51:53 +0200

    [CLUSTER] no_id

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ horizon = 200  # if DEBUG else 200
 batch_size = 32
 eta = 0.1
 grid_size = 8
-agent = "ppo"
+agent = "pg"
 save_interval = 10
 max_steps = int(1e3)
 seed = 984
@@ -34,7 +34,7 @@ device = torch.device("cuda" if use_cuda else "cpu")
 
 RUN_SWEEP = 1
 REMOTE = 1
-NUM_PROCS = 10
+NUM_PROCS = 5
 sweep_yaml = "sweep_seeds.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""  # in host
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
