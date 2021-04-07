75c46d56b84c7ddc0789d3827786e44e115bfb85
d3sm0 <simone.totaro@gmail.com>
2021-04-07 06:43:06 +0200

    [CLUSTER] params_pg

--- config.py
+++ config.py
@@ -11,13 +11,13 @@ pi_lr = 1e-1
 v_lr = 1e-5
 gamma = 0.99
 eps_clip = 0.1
-opt_epochs = 10
+opt_epochs = 20
 horizon = 200  # if DEBUG else 200
-mdp_horizon = 10
+mdp_horizon = 8
 batch_size = 32
-eta = 0.1
+eta = 0.001
 grid_size = 8
-agent = "ppo"
+agent = "pg"
 save_interval = 10
 max_steps = int(1e3)
 seed = 984
@@ -35,10 +35,10 @@ experiment_buddy.register(locals())
 # #SBATCH --mem=24GB
 # """
 
-REMOTE = 0
-RUN_SWEEP = 0
+REMOTE = 1
+RUN_SWEEP = 1
 NUM_PROCS = 5
-sweep_yaml = "sweep_seeds.yaml" if RUN_SWEEP else False
+sweep_yaml = "sweep_params.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""  # in host
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 
