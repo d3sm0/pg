a780d575058aaa146c1e2c606e8f98f361a0ece2
d3sm0 <simone.totaro@gmail.com>
2021-04-08 20:15:16 +0200

    [CLUSTER] no_id

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ opt_epochs = 10
 horizon = 200  # if DEBUG else 200
 eta = 2.
 grid_size = 4
-agent = "pg"
+agent = "ppo"
 save_interval = 10
 max_steps = int(1e3)
 seed = 984
@@ -18,12 +18,12 @@ use_cuda = False
 experiment_buddy.register(locals())
 plot_path = "plots"
 
-REMOTE = 0
-RUN_SWEEP = 0
+REMOTE = 1
+RUN_SWEEP = 1
 NUM_PROCS = 5
 sweep_yaml = "sweep_seeds.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""  # in host
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 
 tb = experiment_buddy.deploy(host=HOST, sweep_yaml=sweep_yaml, proc_num=NUM_PROCS,
-                             wandb_kwargs=dict(mode="disabled"))
+                             wandb_kwargs=dict(mode="disabled" if DEBUG else "online"))
