78a43fc81fcd29fc17ccd02db7e8b7a8e5e6b13d
d3sm0 <simone.totaro@gmail.com>
2021-04-08 18:08:43 +0200

    [CLUSTER] no_id

---
+++ _main.py
@@ -0,0 +1,99 @@
+import itertools
+from shamdp import process_action
+
+import torch
+from gym_minigrid.envs import EmptyEnv
+
+import config
+from agent import PG, PPO
+# from chain_mdp import MDP
+from env_utils import MiniGridWrapper, StatisticsWrapper
+from eval_policy import eval_policy
+
+
+def gather_trajectory(env, model, horizon):
+    state = env.reset()
+    info = {}
+    done = False
+    while not done:
+        action = model.act(state)
+        action = process_action(state, action)
+        s_prime, r, done, info = env.step(action)
+        model.put_data((state, action, r, s_prime, 1 - done))
+        state = s_prime
+        if done:
+            state = env.reset()
+    return info
+
+
+def main():
+    from shamdp import get_shamdp
+    env = EmptyEnv(size=config.grid_size)  # FourRoomsEnv(goal_pos=(12, 16))
+    #env = get_shamdp(horizon=config.mdp_horizon)
+
+    torch.manual_seed(config.seed)
+    # env.seed(config.seed)
+    # env = MDP()
+    env = MiniGridWrapper(env)
+    env = StatisticsWrapper(env)
+    if config.agent == "pg":
+        agent = PG(action_space=env.action_space.n, observation_space=env.env.n_states, h_dim=config.h_dim)
+    else:
+        agent = PPO(action_space=env.action_space.n, observation_space=env.env.n_states, h_dim=config.h_dim)
+    # plot_value(env, agent, global_step=0)
+
+    # writer = tb.SummaryWriter(log_dir=f"logs/{dtm}_as_ppo:{config.as_ppo}")
+    for global_step in itertools.count():
+        info = gather_trajectory(env, agent, config.horizon)
+        config.tb.add_scalar("return", info["env/returns"], global_step=global_step * config.horizon)
+        losses = agent.train()
+        config.tb.add_histogram("pi", agent._agent.pi.data, global_step=global_step * config.horizon)
+        agent.data.clear()
+        for k, v in losses.items():
+            config.tb.add_scalar(k, v, global_step=global_step * config.horizon)
+        if global_step % config.save_interval == 0:
+            path = config.tb.add_object("agent", agent, global_step=0)
+            eval_info = eval_policy(path, config.eval_runs, record_episode=False)
+            for k, v in eval_info.items():
+                config.tb.add_scalar(k, v, global_step=global_step * config.horizon)
+            plot_value(env, agent, global_step * config.horizon)
+        if global_step > config.max_steps:
+            break
+    #env.close()
+    config.tb.run.finish()
+
+
+def plot_value(env, agent, global_step):
+    value = torch.zeros(4, config.grid_size, config.grid_size)
+    q_value = torch.zeros(4, config.grid_size, config.grid_size)
+    pi = torch.zeros(4, config.grid_size, config.grid_size)
+    for z in range(4):
+        for x in range(config.grid_size):
+            for y in range(config.grid_size):
+                idx = env.env._state_to_idx[(z, x, y)]
+                value[z, x, y] = agent._agent.v[idx]
+                q_value[z, x, y] = agent._agent.q[idx].max()
+                pi[z, x, y] = agent._agent.pi.data.argmax()
+
+    value = value.max(0)[0]
+    q_value = q_value.max(0)[0]
+    pi = pi.max(0)[0]
+    # q_value = agent._agent.q.reshape((4, config.grid_size, config.grid_size, 3)).max(-1)[0].max(0)[0]
+
+    import matplotlib.pyplot as plt
+    # fig, ax = plt.subplots(1, 3)
+    # fig = plt.figure()
+    fig = plt.figure()
+    ax = plt.imshow(value)
+    config.tb.add_figure("plots/value", fig, global_step)
+    fig = plt.figure()
+    ax = plt.imshow(q_value)
+    config.tb.add_figure("plots/q_value", fig, global_step)
+    fig = plt.figure()
+    ax = plt.imshow(pi)
+    config.tb.add_figure("plots/pi", fig, global_step)
+    plt.close()
+
+
+if __name__ == '__main__':
+    main()
--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import sys
 import experiment_buddy
 
-pi_lr = 1e-1
 gamma = 0.99
 eps_clip = 0.1
 opt_epochs = 10
@@ -9,7 +8,6 @@ horizon = 200  # if DEBUG else 200
 eta = 2.
 grid_size = 4
 agent = "pg"
-save_interval = 10
 max_steps = int(1e3)
 seed = 984
 
@@ -18,12 +16,13 @@ use_cuda = False
 experiment_buddy.register(locals())
 plot_path = "plots"
 
-REMOTE = 0
+REMOTE = 1
 RUN_SWEEP = 0
-NUM_PROCS = 5
+NUM_PROCS = 1
 sweep_yaml = "sweep_seeds.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""  # in host
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 
 tb = experiment_buddy.deploy(host=HOST, sweep_yaml=sweep_yaml, proc_num=NUM_PROCS,
-                             wandb_kwargs=dict(mode="disabled"))
+                             wandb_kwargs=dict(mode="disabled" if not REMOTE else "enabled")
+                             )
