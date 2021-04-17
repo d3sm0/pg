d0cef6740b235b5d1429ff6af07a44c833b24b6f
d3sm0 <simone.totaro@gmail.com>
2021-04-18 01:55:23 +0200

    [CLUSTER] escort

--- plots.py
+++ plots.py
@@ -30,17 +30,16 @@ for idx, eta in enumerate(etas):
     pi_ppo = pi.clone()
     for _ in range(n_steps):
         pi_ppo, _, e_ppo, v_ppo, _ = get_pi(env, pi_ppo, ppo, eta=eta)
-    ax.hist(np.arange(env.action_space), weights=pi_ppo[0], label=f"agent=ppo:delta:h={e_ppo :.3f}:v={v_ppo[0] :.3f}",
+    ax.hist(np.arange(env.action_space), weights=pi_ppo[0], label=f"ppo:h={e_ppo :.3f}:v={v_ppo[0] :.3f}",
             alpha=0.5)
-    pi_pg = pi.clone()
+    pg_pi = pi.clone()
     for _ in range(n_steps):
-        pi_pg, _, e_pg, v_pg, _ = get_pi(env, pi_pg, pg, eta=eta)
-    ax.hist(np.arange(env.action_space), weights=pi_pg[0], label=f"agent=pg:delta:h={e_pg :.3f}:v={v_pg[0] :.3f}",
-            alpha=0.5)
+        pg_pi, _, e_pg, v_pg, _ = get_pi(env, pg_pi, pg, eta=eta)
+    ax.hist(np.arange(env.action_space), weights=pg_pi[0], label=f"pg:h={e_pg :.3f}:v={v_pg[0] :.3f}", alpha=0.5)
     ax.set_title(f"eta:{eta:.2f}")
     ax.legend()
     plt.xticks(np.arange(env.action_space), labels)
-plt.savefig(f"soft_value_{n_steps}")
+plt.savefig(f"plots/softmax_{n_steps}_{config.grid_size}")
 plt.tight_layout()
 plt.show(block=False)
 plt.pause(5)
