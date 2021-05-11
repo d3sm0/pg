89949dd7fca464c13579bd1b6129cd7218449ec7
d3sm0 <simone.totaro@gmail.com>
2021-05-11 20:04:35 +0200

    [CLUSTER] random_init_vf

---
+++ evaluate_projection.py
@@ -0,0 +1,17 @@
+import jax.numpy as jnp
+
+from mdp import get_n_state_chain
+from utils.misc_utils import ppo, pg, policy_iteration
+
+env = get_n_state_chain(2)
+env.P[1, 0, 0] = 0
+env.P[1, 0, 1] = 1
+env.P[1, 1, 1] = 1
+pi = jnp.ones((2, 2)) / 2
+eta = 1
+e_pg, pg_pi, t, v_pg, ppo_pi, _, _ = policy_iteration(ppo, env, eta, pi)
+e_pg, pg_pi, t, v_pg, pg_pi, _, _ = policy_iteration(pg, env, eta, pi)
+
+print(ppo_pi)
+
+ppo_pi[:, 0, 0] < pg_pi[:, 0, 0]
