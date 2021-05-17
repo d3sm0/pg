ac07d2f2c4ce947d18b9ded858555bea9df1ccd8
d3sm0 <simone.totaro@gmail.com>
2021-05-17 15:02:21 +0200

    [CLUSTER] frozen_lake

--- simple.py
+++ simple.py
@@ -2,40 +2,55 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
 import config
-from experiments.plot_fn import gridworld_plot_sa, plot_policy_at_state, plot_vf
-from utils.mdp import get_gridworld
+from experiments.plot_fn import gridworld_plot_sa, plot_policy_at_state, plot_vf, chain_plot_sa, chain_plot_vf
+from utils.mdp import get_gridworld, get_shamdp
 from utils.envs import get_four_rooms
 from utils.misc_utils import ppo, get_star, pg_clip, policy_iteration, pg
 
 
 def main():
-    # etas = np.linspace(0.1, 1.0, num=9)
-    etas = [1.]
+    # etas = np.linspace(-7., -2., num=9)
+    etas = np.linspace(0.01, 1., num=9)
+    # etas = [1.0]
     env = get_four_rooms(gamma=config.gamma)
-    pi = np.ones(shape=(1, env.action_space))
+    #env = get_shamdp(horizon=20, c=1.6)
+    n_parameters = 1 if config.use_fa else env.state_space
+    pi = np.ones(shape=(n_parameters, env.action_space))
+
+    def stop_criterion(t, v, v_old):
+        return jnp.linalg.norm(v - v_old) < config.eps or t >= config.max_steps
+
     pi /= pi.sum(1, keepdims=True)
     pi = jnp.array(pi)
     # from main import action_to_text
-    labels = ["left", "right", "up", "down"]
-    pi_star, _, _, v_star = get_star(env)
-    # plt.plot(pi_star)
-    gridworld_plot_sa(env, pi_star, f"pi_star")
-    plt.savefig("plots/pi_star")
-    plt.close()
-    agents = {"pg_clip":pg_clip} #{"pg": pg, "pg_clip": pg_clip, "ppo": ppo}
+    labels = ["right", "left", "up", "down"]
+    # labels = ["right", "left_0", "left_1", "left_2"]
+    _, _, _, v_star = get_star(env)
+    ## plt.plot(pi_star)
+    ##gridworld_plot_sa(env, pi_star, f"pi_star")
+    ## chain_plot_sa(env, pi_star, f"pi_star")
+    # plt.savefig("plots/pi_star")
+    # plt.close()
+    # agents = {"pg_clip": pg_clip}
+    agents = {"pg_clip": pg_clip, "ppo": ppo} #, "pg":pg}
     for agent, agent_fn in agents.items():
         fig, axs = plt.subplots(3, 3, figsize=(12, 6))
         axs = axs.flatten()
         for idx, eta in enumerate(etas):
             ax = axs[idx]
-            e_pg, pg_pi, t, v_pg, pi_s, advs, _ = policy_iteration(agent_fn, env, eta, pi)
+            e_pg, pg_pi, t, v_pg, pi_s, advs, _, d_s = policy_iteration(agent_fn, env, eta, pi, stop_criterion)
             title = f"{agent}:h={e_pg :.3f}:gap={(env.p0 * (v_star - v_pg)).sum():.3f}:t={t}:eta={eta:.2f}"
-            plot_policy_at_state(pg_pi, action_label=labels, title=title, ax=ax)
-            # plot_vf(env, v_pg, title, ax=ax, frame=(0, 0, 0, 0))
+            # if config.use_fa:
+            # plot_policy_at_state(pg_pi, action_label=labels, title=title, ax=ax)
+            # else:
+            # gridworld_plot_sa(env, pg_pi, title, ax=ax)
+            plot_vf(env, v_pg, title, ax=ax)
+            # chain_plot_sa(env, pi_s[:, 0, 0], title, ax=ax)
+            # chain_plot_vf(env, v_pg, title, ax=ax)
         # plt.show()
         # plt.legend(["left", "right"])
         plt.tight_layout()
-        plt.savefig(f"plots/frozen_lake/agent={agent}:size={config.grid_size}")
+        plt.savefig(f"plots/frozen_lake/vf:agent={agent}")
         plt.show(block=False)
         plt.pause(5)
         plt.close()
