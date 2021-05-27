40d5235ab6fe95c639f6e7204c240d371bcb4eb2
d3sm0 <simone.totaro@gmail.com>
2021-05-27 12:30:15 +0200

    [CLUSTER] approx

--- config.py
+++ config.py
@@ -2,25 +2,22 @@ import sys
 import os
 import experiment_buddy
 
-pi_lr = 0.01
+use_fa = True
+horizon = 6 if use_fa else 20
+penalty = 0.15 if use_fa else 1.4  #1.6
+eps = 1e-4
 gamma = 0.9
-eps_clip = 0.1
-opt_epochs = 10
-horizon = 19  # if DEBUG else 200
-eta = 1.0
-grid_size = 5
-agent = "pg"
+eta = 1.
+grid_size = 10
+agent = "pg_clip"
 save_interval = 10
-max_steps = int(4e2)
+max_steps = int(50)
 seed = 984
 eval_episodes = 10
 data = "data"
-use_fa = True
-use_kl = False
-eps = 1e-6 if use_kl else 1e-4
 
-REMOTE = 1
-RUN_SWEEP = 1
+REMOTE =  1
+RUN_SWEEP = REMOTE
 NUM_PROCS = 5
 sweep_yaml = "sweep_params.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""  # in host
@@ -29,7 +26,7 @@ DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
 render = not DEBUG
 experiment_buddy.register(locals())
 tb = experiment_buddy.deploy(host=HOST, sweep_yaml=sweep_yaml, proc_num=NUM_PROCS,
-                             wandb_kwargs=dict(mode="disabled" if DEBUG else "online"))
+                             wandb_kwargs=dict(mode= "disabled" if DEBUG else "online"))
 
 os.makedirs(data, exist_ok=True)
 plot_path = os.path.join(tb.objects_path, "plots")
