0767e0c94c25752ca1e9fbd200fa8b5367974d5e
d3sm0 <simone.totaro@gmail.com>
2021-05-31 05:13:39 +0200

    up

--- utils/misc_utils.py
+++ utils/misc_utils.py
@@ -1,31 +1,37 @@
+import itertools
+
 from jax import numpy as jnp
 
 import numpy as np
 
 import config
+import jax
+
+from experiments.plot_fn import gridworld_plot_sa
 
 
-def v_iteration(env, n_iterations=100, eps=1e-5):
+def v_iteration(env, n_iterations=1000, eps=1e-6):
     v = jnp.zeros(env.state_space)
     for _ in range(n_iterations):
         v_new = (env.R + jnp.einsum("xay,y->xa", env.gamma * env.P, v)).max(1)
-        if jnp.linalg.norm(v - v_new) < eps:
-            break
+        # if jnp.linalg.norm(v - v_new) < eps:
+        #    break
         v = v_new.clone()
     return v
 
 
-def q_iteration(env, n_iterations=100, eps=1e-5):
+def q_iteration(env, n_iterations=1000, eps=1e-6):
     q = jnp.zeros((env.state_space, env.action_space))
     for _ in range(n_iterations):
         q_star = q.max(1)
         q_new = (env.R + jnp.einsum("xay,y->xa", env.gamma * env.P, q_star))
-        if jnp.linalg.norm(q - q_new) < eps:
-            break
+        # if jnp.linalg.norm(q - q_new) < eps:
+        #    break
         q = q_new.clone()
     return q
 
 
+@jax.partial(jax.jit, static_argnums=(0,))
 def get_value(env, pi):
     p_pi = jnp.einsum('xay,xa->xy', env.P, pi)
     r_pi = jnp.einsum('xa,xa->x', env.R, pi)
@@ -33,6 +39,7 @@ def get_value(env, pi):
     return v
 
 
+@jax.partial(jax.jit, static_argnums=(0,))
 def get_dpi(env, pi):
     p_pi = jnp.einsum('xay,xa->xy', env.P, pi)
     rho = env.p0
@@ -41,6 +48,7 @@ def get_dpi(env, pi):
     return d_pi
 
 
+@jax.partial(jax.jit, static_argnums=(0,))
 def get_q_value(env, pi):
     v = get_value(env, pi)
     v_pi = jnp.einsum('xay,y->xa', env.P, v)
@@ -49,9 +57,9 @@ def get_q_value(env, pi):
 
 
 def kl_fn(p, q, ds):
-    _kl = ((p+1e-6) * jnp.log((p+1e-6)/(q+1e-6))).sum(1)
+    _kl = ((p + 1e-6) * jnp.log((p + 1e-6) / (q + 1e-6))).sum(1)
     _kl = (ds * _kl).sum()
-    #assert _kl >=0
+    # assert _kl >=0
     return jnp.clip(_kl, 0.)
 
 
@@ -66,7 +74,7 @@ def pg_clip(pi, adv, eta):
     # This number might be negative
     pi = pi * (1 + eta * adv)
     if not is_prob_mass(pi):
-        pi = jnp.clip(pi, a_min=0.)
+        pi = jnp.clip(pi, a_min=1e-3)
         pi = pi / pi.sum(1, keepdims=True)
     return pi
 
@@ -95,13 +103,18 @@ def entropy_fn(pi):
     return - (pi * jnp.log(pi + 1e-6)).sum(1)
 
 
+import jax
+
+
 def get_pi(env, pi_old, pi_fn, eta):
     pi = pi_old.clone()
     v = get_value(env, pi)
     q = get_q_value(env, pi)
     d_s = get_dpi(env, pi)
     adv = q - jnp.expand_dims(v, 1)
+
     if config.use_fa:
+        # d_s = jax.nn.softmax(jax.nn.softmax(adv.max(axis=1))*d_s)
         _adv = jnp.expand_dims(jnp.einsum('i,ij->j', d_s, adv), 0)
     else:
         _adv = adv
@@ -134,36 +147,21 @@ def is_prob_mass(pg_pi):
     return jnp.allclose(pg_pi.sum(1), 1) and (pg_pi.min() >= 0).all()
 
 
-def policy_iteration(agent_fn, env, eta, pi):
-    last_v = get_value(env, pi)
-    t = 0
+import matplotlib.pyplot as plt
+
+
+def policy_iteration(agent_fn, env, eta, pi, stop_criterion):
     pi_old = pi.clone()
-    policies = [pi]
-    advs = [jnp.zeros((env.state_space, env.action_space))]
-    values = [last_v]
-    kls =[jnp.array(0., )]
-    *_, v_star = get_star(env)
-    while True:
+    data = []
+    v_last = jnp.zeros(shape=env.state_space)
+    for step in itertools.count():
         pi, adv, e_pg, v, kl, d_s = get_pi(env, pi_old, agent_fn, eta=eta)
-        kls.append(kl)
-        policies.append(pi)
-        advs.append(adv)
-        values.append(v)
-        if not is_prob_mass(pi):
-            print(f"pi is not valid", pi)
+        print(pi)
+        data.append((pi, adv, v, d_s))
+        if stop_criterion(step, v, v_last) or not is_prob_mass(pi):
             break
-        # plot value function sto
-        #if config.use_kl:
-        #    delta = kl
-        #else:
-        delta = jnp.linalg.norm(v - last_v)
-        eps = config.eps
-        if delta < eps or t > config.max_steps:
-            break
-        t += 1
+        step += 1
         pi_old = pi
-        last_v = v
-    policies = jnp.stack(policies, 0)
-    advs = jnp.stack(advs, 0)
-    values = jnp.stack(values, 0)
-    return e_pg, pi, t, v, policies, advs, values
+        v_last = v.copy()
+    pis, advs, vs, d_s = list(map(lambda x: jnp.stack(x, 0), list(zip(*data))))
+    return e_pg, pi, step, v, pis, advs, vs, d_s
