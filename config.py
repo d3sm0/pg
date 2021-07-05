19eddc5fe5dca9b979ad4c860bd4b404ae642930
d3sm0 <simone.totaro@gmail.com>
2021-07-05 05:41:28 +0200

    sota

--- config.py
+++ config.py
@@ -6,29 +6,30 @@ use_fa = False
 horizon = 6 if use_fa else 20
 penalty = 0.15 if use_fa else 1.4  # 1.6
 eps = 1e-4
-gamma = 0.99
-eta = 0.01
-grid_size = 3
+gamma = 0.90
+eta = 0.1
+grid_size = 5
 opt_epochs = 10
-agent = "pg_clip"
+agent_id = "mdpo"
+env_id = "cliff"
 save_interval = 10
-max_steps =  int(10)
 seed = 984
+mask_prob = 1.
 lr = 1.
-mask_prob = 0.8
+shift = 0.95
 
-REMOTE = 0
+REMOTE = 1
 RUN_SWEEP = REMOTE
 NUM_PROCS = 5
-sweep_yaml = "experiments/sweep.yaml" if RUN_SWEEP else False
+sweep_yaml = "experiments/sweep_seeds.yaml" if RUN_SWEEP else False
 HOST = "mila" if REMOTE else ""  # in host
 DEBUG = '_pydev_bundle.pydev_log' in sys.modules.keys()
+max_steps = int(3e3)
 
 render = not DEBUG
 experiment_buddy.register(locals())
 tb = experiment_buddy.deploy(host=HOST, sweep_yaml=sweep_yaml, proc_num=NUM_PROCS,
-                             wandb_kwargs=dict(mode="disabled")) # if DEBUG else "online")
-
+                             wandb_kwargs=dict(mode="online"))  # "disabled" if DEBUG else "online"))
 
 plot_path = os.path.join(tb.objects_path, "plots")
 os.makedirs(plot_path, exist_ok=True)
